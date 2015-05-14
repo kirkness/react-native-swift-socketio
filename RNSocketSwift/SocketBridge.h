18991a71356400653efb5da3ead8f17306ae232d
Henry Kirkness <Henry@Henrys-MacBook-Pro.local>
2015-05-14 16:55:31 +0100

    Initial commit

---
+++ RNSocketSwift/SocketBridge.h
@@ -0,0 +1,18 @@
+//
+//  SocketBridge.h
+//  ReactSockets
+//
+//  Created by Henry Kirkness on 12/05/2015.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#ifndef ReactSockets_SocketBridge_h
+#define ReactSockets_SocketBridge_h
+
+#import "RCTAssert.h"
+#import "RCTBridge.h"
+#import "RCTLog.h"
+#import "RCTBridgeModule.h"
+#import "RCTEventDispatcher.h"
+
+#endif
---
+++ RNSocketSwift/SocketBridge.m
@@ -0,0 +1,20 @@
+//
+//  SocketBridge.m
+//  ReactSockets
+//
+//  Created by Henry Kirkness on 10/05/2015.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import "RCTBridge.h"
+
+@interface RCT_EXTERN_MODULE(SocketIO, RCTBridge)
+
+RCT_EXTERN_METHOD(initialise:(NSString*)connection config:(NSDictionary*)config)
+RCT_EXTERN_METHOD(addHandlers:(NSDictionary*)handlers)
+RCT_EXTERN_METHOD(connect)
+RCT_EXTERN_METHOD(close:(BOOL)fast)
+RCT_EXTERN_METHOD(reconnect)
+RCT_EXTERN_METHOD(emit:(NSString*)event items:(NSObject*)items)
+
+@end
