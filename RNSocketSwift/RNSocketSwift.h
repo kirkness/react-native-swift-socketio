f053ea8b213d64b0b735bb5e4b16c2ea3bf1e162
Henry Kirkness <Henry@Henrys-MacBook-Pro.local>
2015-05-14 16:42:51 +0100

    Initial Commit

---
+++ RNSocketSwift/RNSocketSwift.h
@@ -0,0 +1,13 @@
+//
+//  RNSocketSwift.h
+//  RNSocketSwift
+//
+//  Created by Henry Kirkness on 14/05/2015.
+//  Copyright (c) 2015 Henry Kirkness. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNSocketSwift : NSObject
+
+@end
---
+++ RNSocketSwift/RNSocketSwift.m
@@ -0,0 +1,13 @@
+//
+//  RNSocketSwift.m
+//  RNSocketSwift
+//
+//  Created by Henry Kirkness on 14/05/2015.
+//  Copyright (c) 2015 Henry Kirkness. All rights reserved.
+//
+
+#import "RNSocketSwift.h"
+
+@implementation RNSocketSwift
+
+@end
